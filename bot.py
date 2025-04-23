eb8297fa00aebdabcbb8e771938546fd3af8f84f
Rusya199518 <Kanubis228@yandex.ru>
2025-04-23 13:17:07 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+echo "# connectme-bot" >> README.md
+git init
+git add README.md
+git commit -m "первый коммит"
+git branch -M main
+git remote add origin https://github.com/Rusya199518/connectme-bot.git
+git push -u origin main
